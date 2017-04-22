adf2f9c60de55619a3785be8c0d0bd5218a39851
Olivier <vil1driver@gmail.com>
2017-04-22 16:13:20 +0200

    update

--- modules.lua
+++ modules.lua
@@ -38,7 +38,6 @@ domoticzPSWD = ''		-- mot de pass
 domoticzPASSCODE = ''	-- pour interrupteur protégés
 domoticzURL = 'http://'..domoticzIP..':'..domoticzPORT
 
-
 admin = 'xxxxx@gmail.com'
 
 --------------------------------
@@ -289,38 +288,111 @@ function ReverseTable(t)
 end
 
 -- affiche le contenu d'une table
-function print_r( t )
-    local print_r_cache={}
-    local function sub_print_r(t,indent)
-        if (print_r_cache[tostring(t)]) then
-            print(indent.."*"..tostring(t))
-        else
-            print_r_cache[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
-                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        print(indent..string.rep(" ",string.len(pos)+6).."}")
-                    elseif (type(val)=="string") then
-                        print(indent.."["..pos..'] => "'..val..'"')
-                    else
-                        print(indent.."["..pos.."] => "..tostring(val))
-                    end
-                end
+--[[  
+
+   Author: Julio Manuel Fernandez-Diaz
+   Date:   January 12, 2007
+   (For Lua 5.1)
+   
+   Modified slightly by RiciLake to avoid the unnecessary table traversal in tablecount()
+
+   Formats tables with cycles recursively to any depth.
+   The output is returned as a string.
+   References to other tables are shown as values.
+   Self references are indicated.
+
+   The string returned is "Lua code", which can be procesed
+   (in the case in which indent is composed by spaces or "--").
+   Userdata and function keys and values are shown as strings,
+   which logically are exactly not equivalent to the original code.
+
+   This routine can serve for pretty formating tables with
+   proper indentations, apart from printing them:
+
+      print(table_show(t, "t"))   -- a typical use
+   
+   Heavily based on "Saving tables with cycles", PIL2, p. 113.
+
+   Arguments:
+      t is the table.
+      name is the name of the table (optional)
+      indent is a first indentation (optional).
+]]
+function table_show(t, name, indent)
+   local cart     -- a container
+   local autoref  -- for self references
+
+   --[[ counts the number of elements in a table
+   local function tablecount(t)
+      local n = 0
+      for _, _ in pairs(t) do n = n+1 end
+      return n
+   end
+   ]]
+   -- (RiciLake) returns true if the table is empty
+   local function isemptytable(t) return next(t) == nil end
+
+   local function basicSerialize (o)
+      local so = tostring(o)
+      if type(o) == "function" then
+         local info = debug.getinfo(o, "S")
+         -- info.name is nil because o is not a calling level
+         if info.what == "C" then
+            return string.format("%q", so .. ", C function")
+         else 
+            -- the information is defined through lines
+            return string.format("%q", so .. ", defined in (" ..
+                info.linedefined .. "-" .. info.lastlinedefined ..
+                ")" .. info.source)
+         end
+      elseif type(o) == "number" or type(o) == "boolean" then
+         return so
+      else
+         return string.format("%q", so)
+      end
+   end
+
+   local function addtocart (value, name, indent, saved, field)
+      indent = indent or ""
+      saved = saved or {}
+      field = field or name
+
+      cart = cart .. indent .. field
+
+      if type(value) ~= "table" then
+         cart = cart .. " = " .. basicSerialize(value) .. ";\n"
+      else
+         if saved[value] then
+            cart = cart .. " = {}; -- " .. saved[value] 
+                        .. " (self reference)\n"
+            autoref = autoref ..  name .. " = " .. saved[value] .. ";\n"
+         else
+            saved[value] = name
+            --if tablecount(value) == 0 then
+            if isemptytable(value) then
+               cart = cart .. " = {};\n"
             else
-                print(indent..tostring(t))
+               cart = cart .. " = {\n"
+               for k, v in pairs(value) do
+                  k = basicSerialize(k)
+                  local fname = string.format("%s[%s]", name, k)
+                  field = string.format("[%s]", k)
+                  -- three spaces between levels
+                  addtocart(v, fname, indent .. "   ", saved, field)
+               end
+               cart = cart .. indent .. "};\n"
             end
-        end
-    end
-    if (type(t)=="table") then
-        print(tostring(t).." {")
-        sub_print_r(t,"  ")
-        print("}")
-    else
-        sub_print_r(t,"  ")
-    end
-    print()
+         end
+      end
+   end
+
+   name = name or "table"
+   if type(t) ~= "table" then
+      return name .. " = " .. basicSerialize(t)
+   end
+   cart, autoref = "", ""
+   addtocart(t, name, indent)
+   return cart .. autoref
 end
 
 -- retourne la table des derniers log (première ligne = dernier log)
