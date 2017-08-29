ea9da08372f749f9e8e292a3c54ce56517fc0868
Olivier <olivier.leger@outlook.com>
2017-08-29 22:16:50 +0200

    Moving settings to proper file

--- modules.lua
+++ modules.lua
@@ -30,20 +30,7 @@ local debug = true  -- true pour voir les logs dans la console log Dz ou false p
 ------ 	  USER SETTINGS	  ------
 --------------------------------
 
--- domoticz
-domoticzIP = '192.168.22.100'	--'127.0.0.1'
-domoticzPORT = '8080'
-domoticzUSER = ''		-- nom d'utilisateur
-domoticzPSWD = ''		-- mot de pass
-domoticzPASSCODE = ''	-- pour interrupteur protégés
-domoticzURL = 'http://'..domoticzIP..':'..domoticzPORT
-
--- passerelle SMS
-smsGatewayIP = '192.168.22.171'
-smsGatewayPORT = '41047'
-smsGatewayURL = 'http://'..smsGatewayIP..':'..smsGatewayPORT
-
-admin = 'xxxxxxxxxxxxx'
+require "./settings.lua"
 
 --------------------------------
 ------         END        ------
---
+++ settings.lua
@@ -0,0 +1,14 @@
+-- domoticz
+domoticzIP = '127.0.0.1'
+domoticzPORT = '8080'
+domoticzUSER = ''		-- nom d'utilisateur
+domoticzPSWD = ''		-- mot de pass
+domoticzPASSCODE = ''	-- pour interrupteur protégés
+domoticzURL = 'http://'..domoticzIP..':'..domoticzPORT
+
+-- passerelle SMS
+smsGatewayIP = '127.0.0.1'
+smsGatewayPORT = '41047'
+smsGatewayURL = 'http://'..smsGatewayIP..':'..smsGatewayPORT
+
+admin = 'xxxxxxxxxxxxx'
